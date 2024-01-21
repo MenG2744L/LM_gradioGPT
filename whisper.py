2db83f72cba40cfc3be98629e24591469f00b5c9
MenG <2744181317@qq.com>
2024-01-21 12:37:24 +0800

    Initial commit

---
+++ whisper.py
@@ -0,0 +1,29 @@
+import gradio as gr
+import os
+import openai
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def process(filepath):
+    audio = open(filepath, "rb")
+    key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = key
+    transcript = openai.Audio.transcribe("whisper-1", audio)
+
+    llm = OpenAI(
+        temperature=1,
+        openai_api_key=key)
+
+    return llm(transcript["text"])
+
+
+if __name__ == "__main__":
+    demo = gr.Interface(
+        fn=process,
+        inputs=gr.Audio(sources="microphone", type="filepath"),
+        outputs=["text"],
+    )
+    demo.launch()
