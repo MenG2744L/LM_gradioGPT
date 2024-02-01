23404627058a72b1faa34215bde5d565ee78938d
MenG <2744181317@qq.com>
2024-02-01 22:26:08 +0800

    add /test/whisper.py

---
+++ test/whisper.py
@@ -0,0 +1,16 @@
+import openai
+
+def transcribe(audio):
+    # os.rename(audio, audio + '.wav')
+    audio_file = open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript['text']
+
+
+# 录音文件转文本的过程
+def process_audio(audio, history=[]):
+    input = transcribe(audio)
+    print(input)
+    if input is None:
+        input = "你好"
+    return input
