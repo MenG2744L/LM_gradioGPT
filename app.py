15113d06c24b1c64f1c8dde7e091c40d3302f899
SihanA <57210715+russhustle@users.noreply.github.com>
2024-02-07 11:37:00 +0900

    Update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-import llm_agent
+import src.llm_agent as llm_agent
 import whisper
 
 
@@ -9,8 +9,7 @@ with gr.Blocks(css="#chatbot{height:800px} .overflow-y-auto{height:800px}") as d
     with gr.Row():
         # 录音功能
         with gr.Row():
-            # 得到音频文件地址
-            audio = gr.Audio(sources="microphone", type="filepath")
+            audio = gr.Audio(sources="microphone", type="filepath")  # 得到音频文件地址
         txt = gr.Textbox(show_label=False, placeholder="Enter text and press enter", elem_id="textbox")
     txt.submit(llm_agent.agent_run, [txt, state], [chatbot, state])
     audio.change(whisper.process_audio, [audio, state], [chatbot, state])
