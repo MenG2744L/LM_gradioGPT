4884e9589a9976bf7032c761846a0adf1ff05867
MenG <2744181317@qq.com>
2024-03-01 16:26:28 +0800

    加入图片

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
 import gradio as gr
-import llm_agent
-import whisper
-
+import openai
+from dotenv import load_dotenv
+from src import whisper
+from src import llm_run
 
 with gr.Blocks(css="#chatbot{height:800px} .overflow-y-auto{height:800px}") as demo:
+    gr.Markdown("# Welcome to GradioGPT! 🌟🚀")
+    gr.Markdown(
+        "An easy to use template. It comes with state and settings managment"
+    )
     chatbot = gr.Chatbot(elem_id="chatbot")
     state = gr.State([])
     with gr.Row():
@@ -12,8 +18,11 @@ with gr.Blocks(css="#chatbot{height:800px} .overflow-y-auto{height:800px}") as d
             # 得到音频文件地址
             audio = gr.Audio(sources="microphone", type="filepath")
         txt = gr.Textbox(show_label=False, placeholder="Enter text and press enter", elem_id="textbox")
-    txt.submit(llm_agent.agent_run, [txt, state], [chatbot, state])
+    txt.submit(llm_run.conversation_run, [txt, state], [chatbot, state])
+
     audio.change(whisper.process_audio, [audio, state], [chatbot, state])
 
 if __name__ == "__main__":
+    load_dotenv(".env")
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
     demo.launch()
---
+++ src/llm_agent.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.agents import (
+    initialize_agent,
+    AgentType,
+    Tool, load_tools
+)
+
+
+def init_llm():
+    load_dotenv()
+    os.environ.get("OPENAI_API_KEY")
+    os.environ.get("SERPAPI_API_KEY")
+
+    llm = OpenAI(
+        max_tokens=2048,
+        temperature=0.7
+    )
+
+    memory = ConversationSummaryBufferMemory(llm=ChatOpenAI(), max_token_limit=2048)
+
+    # default_system_prompt = Path("E:\python-prj\gradioGPT-main\src\prompts\system.prompt").read_text(encoding="utf-8")
+
+    # tools = (
+    #     Tool(
+    #         name="search", func=search,
+    #         description="当你需要回答有关客户订单的问题时非常有用"
+    #     ),
+    #     Tool(
+    #         name="recommend", func=recommend,
+    #         description="在你需要回答有关产品推荐的问题时非常有用"
+    #     ),
+    #     # data_tool,
+    # )
+
+    tools = load_tools(["llm-math", "wikipedia"], llm=llm)
+
+    agent = initialize_agent(
+        tools,
+        llm=llm,
+        agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+        handle_parsing_errors=True,
+        max_iteration=2,
+        memory=memory
+    )
+    return agent
+
+
+def agent_run(input, history=[]):
+    agent = init_llm()
+    history.append(input)
+    output = agent.run(input=input)
+    history.append(output)
+    responses = [(u, b) for u, b in zip(history[::2], history[1::2])]
+    return responses, history
